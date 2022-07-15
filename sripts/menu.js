037aeef2f8c38acb8c3e626708aa38dd5ed6e3de
Rafaella Guimarães Venturini <drirayv@pop-os.localdomain>
2022-07-15 11:39:41 -0300

    adicionando input de quantity

--- sripts/menu.js
+++ sripts/menu.js
@@ -3,3 +3,8 @@ function onClickMenu(){
 	document.getElementById("nav").classList.toggle("change");
 	document.getElementById("menu-bg").classList.toggle("change-bg")
 }
+
+const modal = document.querySelector('.modal-block')
+function openModal(){
+modal.classList.remove('active')
+}
